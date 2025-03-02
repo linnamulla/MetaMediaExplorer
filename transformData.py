798be8d395a5f8513ab01e3b2d1bc4870597b015
t99forever <ybezema@gmail.com>
2025-03-02 14:57:24 +0100

    Completed transformation and filtering of dates, df now sorted by most likely date of picture creation

--- transformData.py
+++ transformData.py
@@ -1,45 +1,42 @@
 from datetime import datetime
 import pandas as pd
 
-from transformation.dateTimeFilter import select_date_time
+from transformation.dateTimeFilter import filterDateTime
 
 def transformData(sourceFolder: str) -> None:
-    df = pd.read_csv(sourceFolder + "\\.mediaMetaData.csv", sep = ";")
+    df: pd.DataFrame = pd.read_csv(sourceFolder + "\\.mediaMetaData.csv", sep = ";")
+
+    # Drop the first column of the dataframe
+    df: pd.DataFrame = df.drop(df.columns[0], axis = 1)
 
     # Reorder the columns that contain date and time
     ## Get the columns of the dataframe
     cols = list(df.columns)
     ## Reorder the columns
-    colsDateTime = cols[7:10] + cols[18:19]
-    cols = cols[0:7] + cols[10:18] + cols[19:]
+    colsDateTime: list = cols[7:10] + cols[18:19]
+    cols: list = cols[0:7] + cols[10:18] + cols[19:]
     cols[7:7] = colsDateTime
-    df = df[cols]
+    df: pd.DataFrame = df[cols]
 
-    # Clean values in the recorded column
+    # Filter the date and time in a new column
+    ## Clean values in the recorded column
     for i in range(len(df["recorded"])):
         try:
             if int(str(df["recorded"][i])[0:4]) > datetime.now().year or int(str(df["recorded"][i])[0:4]) < 2000:
                 df.loc[i, "recorded"] = None
         except ValueError:
             df.loc[i, "recorded"] = None
+    ## Filter the date and time in a new column
+    filterDateTime(df)
+    df["filtered"] = pd.to_datetime(df["filtered"].astype(str).str.replace(":", "-", 2))
 
-    # Add a column for the program and reorder the columns
-    ## Combine the columns for app and software
-    df["program"] = df["app"].astype(str) + "; " + df["Software"].astype(str)
-    ## Reorder the columns
-    cols = list(df.columns)
-    colsAppSoftware = cols[11:12] + cols[23:24] + cols[42:43]
-    cols = cols[0:11] + cols[12:23] + cols[24:42]
-    cols[11:11] = colsAppSoftware
-    df = df[cols]
-
-    # Drop the columns that are empty
-    df = df.dropna(axis = 1, how = "all")
-
-    select_date_time(df)
+    # Set index as the path column and sort ascending by the filtered column
+    df: pd.DataFrame = df.set_index("path")
+    df: pd.DataFrame = df.sort_values(by = "filtered", ascending = True)
 
     # Get the information of the dataframe
     print(df.info(verbose = True, show_counts = True))
+    print(df.head(20))
 
     # Save the dataframe as a csv file
     df.to_csv(path_or_buf = (sourceFolder + "\\.mediaMetaData.csv"), sep = ";", index = False)
--- transformation/dateTimeFilter.py
+++ transformation/dateTimeFilter.py
@@ -1,25 +1,28 @@
 import pandas as pd
 
-def select_date_time(df, datetime_col='DateTime', recorded_col='recorded', modified_col='modified', creation_col='creation', new_col='selected_date'):
+def filterDateTime(df, dateTimeCol='DateTime', recordedCol='recorded', modifiedCol='modified', creationCol='creation', newCol='filtered'):
 
-    selected_dates = []
+    filtered = []
     for index, row in df.iterrows():
         try:
-            datetime_val = int(str(row[datetime_col])[:4]) if pd.notna(row[datetime_col]) else None
-            recorded_val = int(str(row[recorded_col])[:4]) if pd.notna(row[recorded_col]) else None
-            modified_val = int(str(row[modified_col])[:4]) if pd.notna(row[modified_col]) else None
-            creation_val = int(str(row[creation_col])[:4]) if pd.notna(row[creation_col]) else None
+            dateTimeVal = int(str(row[dateTimeCol])[:4]) if pd.notna(row[dateTimeCol]) else None
+            try:
+                recordedVal = int(str(row[recordedCol])[:4]) if pd.notna(row[recordedCol]) else None
+            except KeyError:
+                recordedVal = None
+            modifiedVal = int(str(row[modifiedCol])[:4]) if pd.notna(row[modifiedCol]) else None
+            creationVal = int(str(row[creationCol])[:4]) if pd.notna(row[creationCol]) else None
 
-            if datetime_val:
-                selected_dates.append(row[datetime_col])
-            elif recorded_val and modified_val and recorded_val < modified_val:
-                selected_dates.append(row[recorded_col])
-            elif modified_val and creation_val and modified_val < creation_val:
-                selected_dates.append(row[modified_col])
+            if dateTimeVal:
+                filtered.append(row[dateTimeCol])
+            elif recordedVal and modifiedVal and recordedVal < modifiedVal:
+                filtered.append(row[recordedCol])
+            elif modifiedVal and creationVal and modifiedVal < creationVal:
+                filtered.append(row[modifiedCol])
             else:
-                selected_dates.append(row[creation_col])
+                filtered.append(row[creationCol])
         except ValueError:
-            selected_dates.append(None)
+            filtered.append(None)
 
-    df.insert(7, new_col, selected_dates)
+    df.insert(7, newCol, filtered)
     return df
