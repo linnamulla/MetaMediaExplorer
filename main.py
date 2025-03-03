5715310ec48dfa6d74787ed682bc8f79e69baf61
t99forever <ybezema@gmail.com>
2025-03-03 14:24:48 +0100

    Better error handling

--- main.py
+++ main.py
@@ -3,20 +3,17 @@ from transformData import transformData
 
 if __name__ == "__main__":
     while True:
-        print("\nPlease enter name of source folder:")
+        print("\nPlease enter name of source folder, or type \"break\" to break:")
         sourceFolder: str = input("")
         if sourceFolder == "break":
             print("\nProgram has been broken.\n")
             break
         else:
             try:
-                while True:
-                    try:
-                        extractData(sourceFolder)
-                        transformData(sourceFolder, dropEmptyCols = True, dropFloatCols = True)
-                        break
-                    except OSError as e:
-                        print(str(e), "OSError", "Forcing new attempt...\n", sep = "\n")
+                extractData(sourceFolder)
+                transformData(sourceFolder, dropEmptyCols = True, dropFloatCols = True)
                 break
-            except ValueError:
-                print("\nError: Please enter a valid source folder, or type \"break\" to break.")
+            except OSError as e:
+                print(str(e), "OSError. Likely causes are: (1) files are being downloaded from cloud access and can't be accessed, or (2) the .csv file is opened by the user and can't be overwritten.", sep = "\n")
+            except ValueError as e:
+                print(str(e), "ValueError. Please enter a valid source folder.", sep = "\n")
